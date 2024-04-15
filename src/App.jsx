dc8115c69cae09dd0af4b6a19e32cd10351c3078
DineshP02 <dine3hk@gmail.com>
2024-04-15 19:51:44 +0530

    style change

--- src/App.jsx
+++ src/App.jsx
@@ -342,12 +342,15 @@ function App() {
           </div>
         </div>
 
-        <div className="iframe-container">
+        <div style={{ maxWidth: "100%", overflowX: "hidden" }}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d24178.41272818845!2d-74.006138!3d40.755391!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2574ef5fe2c17%3A0x9048dbbcd614a637!2sThe%20Home%20Depot!5e0!3m2!1sen!2sus!4v1713008013204!5m2!1sen!2sus"
-            style={{ border: 0, width: "100%", height: "400px" }}
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d24194.89157059023!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1713187871699!5m2!1sen!2sus"
+            width="800"
+            height="300"
+            style={{ border: "0", maxWidth: "100%" }}
             allowFullScreen
             loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </div>
